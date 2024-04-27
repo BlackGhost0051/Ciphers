c4f79ee1c170b2c341798b3cc10be11e123c8406
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-04-27 21:03:09 +0200

    Add structure

--- Rail-fence-cipher/main.c
+++ Rail-fence-cipher/main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+void logo(){
+
+}
+
 int main(){
+  logo();
+
+  int start = 1;
+  int value;
+  char inputFileName[] = "input.txt";
+
+  while(start){
+    printf("\n| MENU |\n");
+    printf(" 0 -> Exit\n\n");
+
+    printf("Enter value : ");
+    scanf("%d", &value);
+
+    switch(value){
+      case 0:
+        start = 0;
+        break;
+      default: 
+        printf("Enter 0 !!!\n");
+        break;
+    }
+  }
+
   return 0;
 }
