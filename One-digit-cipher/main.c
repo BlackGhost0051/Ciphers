887f00673d265f31edb29db0861c35e625849951
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-18 14:46:22 +0200

    Mod decrypt

--- One-digit-cipher/main.c
+++ One-digit-cipher/main.c
@@ -5,11 +5,30 @@ void logo(){
 }
 
 void decrypt(const char* targetSymbol ,const char* inputFileName){
+    int i = 0;
+    char c;
+    //char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     FILE *inputFile, *outputFile;
+    
 
     inputFile = fopen(inputFileName, "r");
     outputFile = fopen("decrypted.txt", "w");
 
+    if(inputFile == NULL){
+        printf("Error open input file!");
+        return;
+    }
+
+    while ((c = fgetc(inputFile)) != EOF){
+        if(c == '\n'){
+            fputc(alphabet[(i - 1) % 26], outputFile);
+            i = 0;
+        } else if (c == *targetSymbol){
+            i++;
+        }
+    }
+    
 
     fclose(inputFile);
     fclose(outputFile);
@@ -39,6 +58,7 @@ int main(){
                 break;
             case 2:
                 printf(" Target Symbol for decrypt = ");
+                while ((getchar()) != '\n');
                 scanf("%c", &targetSymbol);
                 decrypt(&targetSymbol, inputFileName);
                 break;
