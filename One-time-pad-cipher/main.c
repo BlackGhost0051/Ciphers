6137a6147927fb13c766dc7b4b5ba8e3ed1ed64f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-05-20 10:55:58 +0200

    Add genKeyFile

--- One-time-pad-cipher/main.c
+++ One-time-pad-cipher/main.c
@@ -1,5 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; // Uncomment if you need a lowercase alphabet
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void logo(){
+
+}
+
+void encrypt(){
+
+}
+
+void decrypt(){
+
+}
+
+void genKeyFile(const char *inputFileName, const char *keyGenFileName){
+    int i;
+    char c;
+    FILE *inputFile, *keyGenFile;
+
+    inputFile = fopen(inputFileName, "r");
+    keyGenFile = fopen(keyGenFileName, "w");
+
+    if(inputFile == NULL){
+        printf("Error open input file!");
+        return;
+    }
+
+    srand(time(NULL));
+
+    while((c = fgetc(inputFile)) != EOF){
+        if( 'A' <= c && c <= 'Z' ){
+            int randomNumber = (rand() % 26);
+            fputc(alphabet[randomNumber], keyGenFile);
+        } else if('a' <= c && c <= 'z'){
+            int randomNumber = (rand() % 26);
+            fputc(alphabet[randomNumber], keyGenFile);
+        } else {
+            fputc(c, keyGenFile);
+        }
+    }
+
+    fclose(inputFile);
+    fclose(keyGenFile);
+
+    printf("\nKey generated in %s\n", keyGenFileName);
+}
 
 int main(){
+    logo();
+
+    char inputFileName[] = "input.txt";
+    char keyFileName[] = "key.txt";
+    char keyGenFileName[] = "keyGen.txt";
+
+    int start = 1;
+    int value;
+
+
+    while (start){
+        printf("\n| MENU |\n");
+        printf(" 1 -> Encrypt\n");
+        printf(" 2 -> Decrypt\n");
+        printf(" 3 -> Generate KEY\n");
+        printf(" 0 -> Exit\n\n");
+
+        printf("Enter value : ");
+        scanf("%d", &value);
+
+
+        switch (value){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            genKeyFile(inputFileName, keyGenFileName);
+            break;
+        case 0:
+            start = 0;
+            break;
+        default:
+            printf("Enter 1 , 2 or 0 !!!\n");
+            break;
+        }
+    }
+
     return 0;
 }
